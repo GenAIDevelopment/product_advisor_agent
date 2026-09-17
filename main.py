a0944c937093e4ef02c999dc25fdd1b97e3433e7
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-09-17 08:54:03 +0530

    Established connection with gcp hosted gemini model

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from product-advisor-agent!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,33 @@
+from langchain.chat_models import BaseChatModel, init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+
+def get_model(model: str, **args:dict) -> BaseChatModel:
+    return init_chat_model(
+        model=model,
+        **args
+    )
+
+def get_model_from_gcp() -> ChatGoogleGenerativeAI:
+    return ChatGoogleGenerativeAI(
+        model=os.getenv('MODEL_NAME', "gemini-3.5-flash-lite"),
+        project=os.getenv('PROJECT_ID')
+    )
+
+
+
+
+if __name__ == "__main__":
+    # lets test model
+    load_dotenv()
+    llm = get_model(
+        model=os.getenv('MODEL_NAME', "gemini-3.5-flash-lite"),
+        model_provider="google_genai",
+        project=os.getenv('PROJECT_ID')
+    )
+    result = llm.invoke("What is captial of France")
+    result.pretty_print()
+    
+
